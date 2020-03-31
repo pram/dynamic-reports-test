8276903f28dd5b285cfe2a06e1ec7abbae79db82
Pram <pram@attale.com>
2020-03-31 15:16:34 +0100

    add simple web app

---
+++ src/main/java/com/pram/DynamicReportsTestApplication.java
@@ -0,0 +1,13 @@
+package com.pram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicReportsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicReportsTestApplication.class, args);
+	}
+
+}
