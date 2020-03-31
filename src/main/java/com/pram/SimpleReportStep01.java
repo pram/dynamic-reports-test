1115a869caf433425c26c9299afbc0e95971de09
Pram <pram@attale.com>
2020-03-31 11:40:20 +0100

    adding simple report step

---
+++ src/main/java/com/pram/SimpleReportStep01.java
@@ -0,0 +1,51 @@
+package com.pram;
+
+import net.sf.dynamicreports.report.datasource.DRDataSource;
+import net.sf.dynamicreports.report.exception.DRException;
+import net.sf.jasperreports.engine.JRDataSource;
+
+import java.math.BigDecimal;
+
+import static net.sf.dynamicreports.report.builder.DynamicReports.cmp;
+import static net.sf.dynamicreports.report.builder.DynamicReports.col;
+import static net.sf.dynamicreports.report.builder.DynamicReports.report;
+import static net.sf.dynamicreports.report.builder.DynamicReports.type;
+
+public class SimpleReportStep01 {
+
+    public SimpleReportStep01() {
+        build();
+    }
+
+    public static void main(String[] args) {
+        new SimpleReportStep01();
+    }
+
+    private void build() {
+        try {
+            report()// create new report design
+                    .columns(// add columns
+                            // title, field name data type
+                            col.column("Item", "item", type.stringType()), col.column("Quantity", "quantity", type.integerType()), col.column("Unit price", "unitprice", type.bigDecimalType()))
+                    .title(cmp.text("Getting started"))// shows report title
+                    .pageFooter(cmp.pageXofY())// shows number of page at page footer
+                    .setDataSource(createDataSource())// set datasource
+                    .show(); // create and show report
+        } catch (DRException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JRDataSource createDataSource() {
+        DRDataSource dataSource = new DRDataSource("item", "quantity", "unitprice");
+        dataSource.add("Notebook", 1, BigDecimal.valueOf(500));
+        dataSource.add("DVD", 5, BigDecimal.valueOf(30));
+        dataSource.add("DVD", 1, BigDecimal.valueOf(28));
+        dataSource.add("DVD", 5, BigDecimal.valueOf(32));
+        dataSource.add("Book", 3, BigDecimal.valueOf(11));
+        dataSource.add("Book", 1, BigDecimal.valueOf(15));
+        dataSource.add("Book", 5, BigDecimal.valueOf(10));
+        dataSource.add("Book", 8, BigDecimal.valueOf(9));
+        return dataSource;
+    }
+}
